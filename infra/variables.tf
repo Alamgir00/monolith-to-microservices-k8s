18d515f54a65237da4b3863fe5a5d2e927b57edc
Alamgir00 <46954227+Alamgir00@users.noreply.github.com>
2025-10-28 02:40:37 +0530

    Add variables for region, project, and cluster settings

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,5 @@
-
+variable "region" { type = string default = "us-east-1" }
+variable "project" { type = string default = "mono2micro" }
+variable "cluster_name" { type = string default = "mono2micro-cluster" }
+variable "node_group_name" { type = string default = "mono2micro-ng" }
+variable "node_count" { type = number default = 2 }
