7a4c557ff79e0b2ef2d18bf54454b1a1b563ec3b
Alamgir00 <46954227+Alamgir00@users.noreply.github.com>
2025-10-28 02:28:02 +0530

    Implement basic Flask app with routes

--- services/service-b/app.py
+++ services/service-b/app.py
@@ -1,1 +1,16 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return {"service":"service-b", "message":"ok"}, 200
+
+@app.route("/info")
+def info():
+    return {"service":"service-b", "message":"hello from service-b"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
